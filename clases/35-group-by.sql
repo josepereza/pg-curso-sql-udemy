4e61b469a263a52b3ed1c956bfe8f2dbdd65f496
josepereza <48152618+josepereza@users.noreply.github.com>
2026-02-08 10:51:26 +0100

    Enhance SQL with follower statistics calculations
    Added queries to calculate user follower statistics with and without rounding.

--- clases/35-group-by.sql
+++ clases/35-group-by.sql
@@ -9,6 +9,25 @@ SELECT
 FROM
 	users;
 
+-- Lo mismo pero convirtiendo el SUM a numeric para que nos salgan los decimales.
+SELECT
+	count(*) AS total_users,
+	MIN( followers ) as min_followers,
+	MAX( followers ) as max_followers,
+	AVG(followers) as avg_followers_sin_redondear,
+	ROUND(  AVG( followers ) ) as avg_followers,
+	SUM(followers) / count(*) as avg_manual,
+	
+	-- para que salgan los decimales  y salga lo mismo que con AVG .
+    SUM(followers)::numeric / count(*) as avg_manual_sin_redondear,
+	ROUND(SUM(followers)::numeric / count(*)) as avg_manual3
+                     --sum(followers) * 1.0 / count(*) // esta forma tambien es valida. 
+
+
+FROM
+	users;	
+
+
 
 select first_name, last_name, followers 
 from users
